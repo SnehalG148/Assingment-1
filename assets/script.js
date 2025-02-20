98343a96256c3bd426c1c58bcdfa7ee69b0b01be
Snehal Rodi <snehal.rodi@waiin.com>
2025-02-20 17:24:04 +0530

    first commit

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// const sidebarToggle = document.querySelector("#sidebar-toggle");
+// sidebarToggle.addEventListener("click",function(){
+//     document.querySelector("#sidebar").classList.toggle("collapsed");
+// });
+
+
+const toggler = document.querySelector(".btn");
+toggler.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed")
+}); 
